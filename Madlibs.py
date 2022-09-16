c2060a42ebad28cd50b44271b1aa53f50f29ac96
Rishit Negi <rishitnegi2001@gmail.com>
2022-09-16 13:38:54 +0530

    Update Madlibs.py

--- Madlibs.py
+++ Madlibs.py
@@ -1,22 +1,19 @@
 
-Noun1 = input("Jewellery: ")
-Noun2 = input("Home Decoration: ")
+Noun1 = input("Noun: ")
+Noun2 = input("Noun: ")
 Noun3 = input("Place: ")
-Verb1 = input("Action: ")
+Verb1 = input("Verb: ")
 Noun4 = input("Thing: ")
 Noun5 = input("Furniture: ")
-Verb2 = input("Action: ")
+Verb2 = input("Verb: ")
 Noun6 = input("Body Part: ")
-
+Adj = input("Adjective: ")
+Verb3 = input("Verb: ") 
 
 
 madlib = f'''There were {Noun1} in the {Noun2} right past the door of my {Noun3}. \n
 I wasn’t supposed to {Verb1} the glimmering {Noun4} as they hung, \n
 but I would push up a {Noun5} and {Verb2} on the rickety wood to \n
-brush them with my {Noun6} anyway. It was addictive to paint the \n
-lines of my fingerprints along the surface. I would flick on the \n
-lights and watch them sway, imagining my mother on the dance floor, \n
-conjuring images of date nights when Mama walked out of the house \n
-with a black cocktail dress and her best pearls along with the smokey \n
-scent of her expensive perfume.'''
+brush them with my {Noun6} anyway. It was {Adj} to {Verb3} the \n
+lines of my fingerprints along the surface.'''
 print(madlib)
